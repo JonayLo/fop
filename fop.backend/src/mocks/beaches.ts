3833b28060059753c1edf0e8a791e3273deb6a68
Abraham Romero Quesada <abraham@theagilemonkeys.com>
2019-06-08 14:49:21 +0100

    Add more beaches to the response

--- fop.backend/src/mocks/beaches.ts
+++ fop.backend/src/mocks/beaches.ts
@@ -6,5 +6,25 @@ export const mockBeaches: Beach[] = [
     id: uuid.v4(),
     name: "Las Canteras",
     peopleCleaningToday: 3
+  },
+  {
+    id: uuid.v4(),
+    name: "El Confital",
+    peopleCleaningToday: 6
+  },
+  {
+    id: uuid.v4(),
+    name: "San Cristobal",
+    peopleCleaningToday: 0
+  },
+  {
+    id: uuid.v4(),
+    name: "La Laja",
+    peopleCleaningToday: 8
+  },
+  {
+    id: uuid.v4(),
+    name: "Alcaravaneras",
+    peopleCleaningToday: 3
   }
 ]
