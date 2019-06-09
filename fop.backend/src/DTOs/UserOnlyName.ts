9ddc2aff9ea0df4c5b02d1cf4296f99fe751ad04
Abraham Romero Quesada <abraham@theagilemonkeys.com>
2019-06-09 11:25:33 +0100

    Get images for users from the API

--- fop.backend/src/DTOs/UserOnlyName.ts
+++ fop.backend/src/DTOs/UserOnlyName.ts
@@ -4,12 +4,14 @@ export interface UserOnlyName {
     id: string;
     firstName: string;
     lastName: string;
+    avatarUrl: string;
 }
 
 export const toUserOnlyName = (user: User): UserOnlyName => {
     return {
-       id: user.id,
-       firstName: user.firstName,
-       lastName: user.lastName
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        avatarUrl: user.avatarUrl
     };
 }
