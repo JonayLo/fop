0657ea693347aa4aafa52bfe2f14886a30c71f74
Abraham Romero Quesada <abraham@theagilemonkeys.com>
2019-06-09 14:23:56 +0100

    Some fixes

--- fop.backend/src/mocks/cleaningEvents.ts
+++ fop.backend/src/mocks/cleaningEvents.ts
@@ -7,5 +7,19 @@ export const mockCleaningEvents: CleaningEvent[] = [
         startingDate: new Date(2019, 6, 20, 9),
         durationInHours: 1,
         userId: "adc91d90-fbf6-436f-81d7-2df43f159933"
+    },
+    {
+        id: "c2f42158-61b7-44dd-b0da-24f20c4a3975",
+        beachId: "fe8cb0eb-abe0-4788-8fa8-ceada70118f2",
+        startingDate: new Date(2019, 7, 5, 16),
+        durationInHours: 1,
+        userId: "adc91d90-fbf6-436f-81d7-2df43f159933"
+    },
+    {
+        id: "c2f42158-61b7-44dd-b0da-24f20c4a3975",
+        beachId: "fe8cb0eb-abe0-4788-8fa8-ceada70118f2",
+        startingDate: new Date(2019, 7, 5, 16),
+        durationInHours: 1,
+        userId: "b944448c-1f17-4f75-bb33-d0c815536fdd"
     }
 ]
